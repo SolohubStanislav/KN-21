40a12ee786796e2021e6cc527f73eb130768efaf
SolohubStanislav <solohubsv@lnup.edu.ua>
2024-11-05 14:32:17 +0200

    goodday.py

---
+++ 2/goodday.py
@@ -0,0 +1,9 @@
+while True :
+    msg=input("Please input a msg")
+    if msg=="Добрий день":
+        print("Добрий день")
+    elif msg=="Слава Україні":
+        print("Героям СЛАВА")
+    elif msg=="pa pa":
+        print("the eng")
+        break
